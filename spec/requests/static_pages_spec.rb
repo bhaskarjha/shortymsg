322b3cfa26de53bede2f2de3d99189abbfb6dbf6
Bhaskar Jha <bhaskar.bjha@gmail.com>
2012-03-16 19:33:33 +0530

    Added the .gitignore file

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,9 @@ require 'spec_helper'
 
 describe "Static pages" do
   
+  # let is the Rspec function that stores argument value in a variable 
+  let(:base_title) {"Ruby on Rails Tutorial Sample App |"}
+ 
   # tests the home page
   describe "Home page" do
     it "should have the content 'Sample App'" do
@@ -12,7 +15,7 @@ describe "Static pages" do
     it "should have title 'Home'" do
       visit '/static_pages/home'
       page.should have_selector('title', 
-                    :text =>"Ruby on Rails Tutorial Sample App | Home")
+                    :text => "#{base_title} Home")
     end
     
   end
@@ -27,7 +30,7 @@ describe "Static pages" do
     it "should have the title 'Help'" do
       visit '/static_pages/help'
       page.should have_selector('title',
-                      :text => "Ruby on Rails Tutorial Sample App | Help")
+                      :text => "#{base_title} Help")
     end
    end
    
@@ -41,10 +44,23 @@ describe "Static pages" do
     it "should have title 'About Us'" do
       visit '/static_pages/about'
       page.should have_selector('title',
-                        :text => "Ruby on Rails Tutorial Sample App | About Us")
+                        :text => "#{base_title} About Us")
     end
-    
    end
+   
+   # test the Contact page
+   describe "Contact page" do
+        it "should have the content 'Contact Us'" do
+            visit '/static_pages/contact'
+            page.should have_selector('h1', :text=> "Contact Us")
+        end
+        
+        it "should have title 'Contact Us'" do
+            visit '/static_pages/contact'
+            page.should have_selector('title', 
+                            :text => "#{base_title} Contact Us")
+        end
+   end 
 end
 
   
