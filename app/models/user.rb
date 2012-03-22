e2813e704f6fedaff67d2e6060b9391b1262141d
Bhaskar Jha <bhaskar.bjha@gmail.com>
2012-03-22 20:01:04 +0530

    Make a basic user security feature

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,11 +10,18 @@
 #
 
 class User < ActiveRecord::Base
-    attr_accessible :name, :email, :password_digest
+    attr_accessible :name, :email, :password_digest, 
+                    :password, :password_confirmation
+    
+    has_secure_password
     
     # validation rules
     validates :name, presence: true, length: { maximum: 50 }
+    
     EMAIL_REGEX_EXPRESSION = /\A[\w+\-_]+[\.]?[\w+]+@[a-zA-Z\d\-.]+\.[a-zA_Z]+{2,}\z/i
     validates :email, presence: true, format: { with: EMAIL_REGEX_EXPRESSION },
                       uniqueness: { case_sensitive: false }
+    
+    validates :password, length: { minimum: 6 }
+    validates :password_confirmation, presence: true
 end
